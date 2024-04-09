f633454dee2e45b75c75421294c165b353dd9d42
onresize <embrancelt@outlook.com>
2024-04-09 23:15:49 +0800

    单独拆分sandbox组件

---
+++ docs/.vuepress/components/DIframe.vue
@@ -0,0 +1,28 @@
+<script setup>
+// defineProps defineEmits defineExpose withDefaults
+import { reactive } from 'vue'
+
+const props = defineProps({
+  iframeSrc: ''
+})
+
+</script>
+ 
+<template>
+  <iframe :src="'https://onresize.github.io/react-ts-vite-admin/SandPack?codeType=' + props.iframeSrc" class="iframe-box"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midipayment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>
+</template>
+ 
+<style scoped lang='scss'>
+.iframe-box {
+  width: 100%;
+  height: 500px;
+  border: 0;
+  border-radius: 4px;
+  overflow: hidden;
+  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+  border: 1px solid #e4e7ed;
+  border-radius: 10px;
+}
+</style>
