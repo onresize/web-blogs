2dae0095ad5a650d7a2be73bf62568779540ce58
onresize <embrancelt@outlook.com>
2024-06-13 13:17:37 +0800

    调整

--- docs/.vuepress/public/static/js/auto-upgrade.js
+++ docs/.vuepress/public/static/js/auto-upgrade.js
@@ -3,29 +3,24 @@ void (function () {
   s = document.createElement('script')
   s.type = 'text/javascript'
   s.id = 'version-polling'
-  setTimeout(() => {
-    console.log('是否为github:', location.href.includes('github.io'))
+  s.src =
+    'https://web-blogs-embrance-t-59e6df9d980ba7c216f8993005a68b570df639f055.gitlab.io/static/js/version-polling.js'
 
-    s.src = location.href.includes('github.io')
-      ? '/web-blogs/static/js/version-polling.js'
-      : '/static/js/version-polling.js'
-
-    t = document.getElementsByTagName('script')[0]
-    t.parentNode.appendChild(s, t)
-    s.onload = function () {
-      VersionPolling.createVersionPolling({
-        appETagKey: '__APP_ETAG__',
-        pollingInterval: 5 * 1000,
-        silent: globalThis?.location?.port == '3080', // 3080端口下不检测
-        onUpdate: (self) => {
-          const result = confirm('页面有更新，点击确定刷新页面！')
-          if (result) {
-            self.onRefresh()
-          } else {
-            self.onCancel()
-          }
-        },
-      })
-    }
-  }, 1e3)
+  t = document.getElementsByTagName('script')[0]
+  t.parentNode.appendChild(s, t)
+  s.onload = function () {
+    VersionPolling.createVersionPolling({
+      appETagKey: '__APP_ETAG__',
+      pollingInterval: 5 * 1000,
+      silent: globalThis?.location?.port == '3080', // 3080端口下不检测
+      onUpdate: (self) => {
+        const result = confirm('页面有更新，点击确定刷新页面！')
+        if (result) {
+          self.onRefresh()
+        } else {
+          self.onCancel()
+        }
+      },
+    })
+  }
 })()
